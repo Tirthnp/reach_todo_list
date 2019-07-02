01f6cd11003d89a5be4cd83a20b994b4b9e733dd
Tirth Patel <tirthnp@live.com>
2019-07-02 13:16:26 -0230

    Updates

---
+++ precache-manifest.0b4ac1870ceb8b1762799f1253f5ba82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7c5591267a88251c1ec8a9226b551f",
+    "url": "/reach_todo_list/index.html"
+  },
+  {
+    "revision": "1fbb979dee7e42256f2d",
+    "url": "/reach_todo_list/static/css/main.298ff9cd.chunk.css"
+  },
+  {
+    "revision": "d4e9a3eb25ee02efd491",
+    "url": "/reach_todo_list/static/js/2.83cbff7a.chunk.js"
+  },
+  {
+    "revision": "1fbb979dee7e42256f2d",
+    "url": "/reach_todo_list/static/js/main.7a8ccf2e.chunk.js"
+  },
+  {
+    "revision": "2a844996b841329d7e95",
+    "url": "/reach_todo_list/static/js/runtime~main.bfef0bc1.js"
+  }
+]);
